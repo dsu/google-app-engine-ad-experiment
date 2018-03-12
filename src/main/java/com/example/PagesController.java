dba8af9a5ac6e4399a7516f2d70af19efbd798a1
Dsu <damian.suchodolski@gmail.com>
2018-03-12 21:31:40 +0100

    reklamy

--- src/main/java/com/example/PagesController.java
+++ src/main/java/com/example/PagesController.java
@@ -1,6 +1,13 @@
 package com.example;
 
+import java.util.Random;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,18 +15,53 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class PagesController {
 
 	@RequestMapping("/")
-	public String zgoda() {
+	public String zgoda(@CookieValue(value = "enabled", required = false) String enabledCookieVal) {
+
+		if (enabledCookieVal != null) {
+			return "already";
+		}
+
 		return "zgoda";
 	}
 
 	@GetMapping("/intro")
-	public String intro() {
+	public String intro(@CookieValue(value = "enabled", required = false) String enabledCookieVal) {
+
+		if (enabledCookieVal != null) {
+			return "already";
+		}
+
 		return "intro";
 	}
 
 	@GetMapping("/tekst")
-	public String tekst() {
-		return "tekst";
+	public String tekst(HttpServletResponse response, Model model,
+			@CookieValue(value = "enabled", required = false) String enabledCookieVal) {
+
+		if (enabledCookieVal == null) {
+			enabledCookieVal = String.valueOf(getRand());
+			response.addCookie(new Cookie("enabled", enabledCookieVal));
+		}
+
+		if (enabledCookieVal.equals("1")) {
+			model.addAttribute("ad", true);
+		} else {
+			model.addAttribute("ad", false);
+		}
+
+		return "tekst-teklamy";
+	}
+
+	public static int getRand() {
+
+		Random random = new Random();
+		boolean res = random.nextBoolean();
+
+		if (res)
+			return 1;
+		else
+			return 0;
+
 	}
 
 }
